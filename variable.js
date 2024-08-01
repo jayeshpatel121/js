22839cb1460a5b1fe4d0fe388ccba321a260c006
jayeshpatel121 <113369053+jayeshpatel121@users.noreply.github.com>
2024-08-01 17:19:24 +0000

    variable

---
+++ variable.js
@@ -0,0 +1,17 @@
+const accountId = 123
+let accountName = "jay"
+var accountEmail = "jay@google.com"
+accountcity = "bhuj"
+let accountState;
+
+//  accountId = 3
+
+accountName = "hello"
+accountEmail = "hello@gma"
+accountcity = "kutch"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountcity);
+console.log(accountName);
+
+console.table([accountId,accountEmail,accountName,accountcity,accountState]);

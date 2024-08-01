57af3f0c2b11fb267284b79134cda04636af9900
jayeshpatel121 <113369053+jayeshpatel121@users.noreply.github.com>
2024-08-01 17:21:44 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+"Use strict"; //treat all JS code as newer version
+
+     // alert(3+3)
+
+console.log(3+3) 
+ console.log("jay");    
+
+ let name = "hello"
+ let age = 34
+ let isLoggedIn = true
+ let hello = ""
+
+ console.log(typeof hello);
+ console.log(typeof undefined);
+ console.log(typeof null);
